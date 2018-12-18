ef93a0439ee5caf9b9eff29cad2486bed0b7d661
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-18 19:10:44 +0900

    Init firebase Auth, Store

---
+++ src/firebase/index.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase';
+import {firebaseConfig} from './config.js';
+
+export const firebaseApp=firebase.initializeApp(firebaseConfig);
+export const firebaseAuth=firebase.auth;
+export const fireStore=firebase.firestore();
