99595b6c73bc5f748a23fed7847844bef9bcc3db
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-18 18:03:38 +0900

    apply redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,23 @@
-let nextTodoId = 0
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
-})
-export const toggleTodo = id => ({
-  type: 'TOGGLE_TODO',
-  id
-})
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-})
+
+let nextTodoId = 0;
+export const addTodo = text => dispatch => {
+  return dispatch({
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  })
+}
+
+export const toggleTodo = id => dispatch => {
+  return dispatch({
+    type: 'TOGGLE_TODO',
+    id
+  })
+}
+
+export const setVisibilityFilter = filter => dispatch =>{
+  return dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  })
+}
